f5073977624520ef91c2180f5dcf03fcb5ec6060
Chisaneme Aloni <chisanemealoni@gmail.com>
2023-09-05 18:54:38 +0100

    Call me Moby

--- javascript-warm_up/101-call_me_moby.js
+++ javascript-warm_up/101-call_me_moby.js
@@ -1,8 +1,8 @@
-function asset(x, itsFunction) {
+function callMeMoby(x, itsFunction) {
   for (let i = 0; i < x; i++) {
     itsFunction();
   }
 }
 
 //Export the function to enable visibility outside
-module.exports.asset = asset;
+module.exports.callMeMoby = callMeMoby;
