bfafc942b7453387e1c3aceb2e6ba68a4bc175fb
Chisaneme Aloni <chisanemealoni@gmail.com>
2023-09-06 10:58:47 +0100

    Rectangle #2

---
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,12 @@
+// create a class with its constructor
+class Rectangle {
+  constructor(w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+}
+
+// create a module for exports
+module.exports = Rectangle;
