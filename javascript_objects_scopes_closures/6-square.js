4fb526d1dcee480948be1706ad36b14a6a765fa2
Chisaneme Aloni <chisanemealoni@gmail.com>
2023-09-06 22:07:47 +0100

    Square #1

--- javascript_objects_scopes_closures/6-square.js
+++ javascript_objects_scopes_closures/6-square.js
@@ -9,6 +9,9 @@ class Square extends newSquare {
 
   //this will print the 'c' character
   charPrint(c) {
+    if (c === undefined) {
+      c = "X";
+    }
     for (let i = 0; i < this.height; i++) {
       let row = "";
       for (let j = 0; j < this.width; j++) {
