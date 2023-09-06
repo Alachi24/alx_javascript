c91e47c44fe85334c83925457fc77c78b45b1b44
Chisaneme Aloni <chisanemealoni@gmail.com>
2023-09-06 17:58:41 +0100

    Square #0

---
+++ javascript_objects_scopes_closures/5-square.js
@@ -0,0 +1,9 @@
+// create a class Square that inherits from Rectangle in "4-rectangle.js"
+class Square extends Rectangle {
+  constructor(size) {
+    super(size);
+  }
+}
+
+//create a module for export
+module.exports = Square;
