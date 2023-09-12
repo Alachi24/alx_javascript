ee200baaf9eb0549c747f2d8d165dbd410ec83a0
Chisaneme Aloni <chisanemealoni@gmail.com>
2023-09-12 10:22:29 +0100

    Status code

--- javascript-network/0-statuscode.js
+++ javascript-network/0-statuscode.js
@@ -1,9 +1,10 @@
 #!/usr/bin/node
-
+//create a const of request and url
 const req = require("request");
 
-const url = "https://intranet.alxswe.com/";
+const url = "";
 
+//using "get" to access the url from the source
 req
   .get(url, { encoding: "utf-8" })
 
