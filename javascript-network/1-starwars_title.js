41fbf5ec890b07c3b7402123c074157855ab700c
Chisaneme Aloni <chisanemealoni@gmail.com>
2023-09-12 19:29:02 +0100

    Star wars movie title

--- javascript-network/1-starwars_title.js
+++ javascript-network/1-starwars_title.js
@@ -10,7 +10,7 @@ const url = `https://swapi-api.alx-tools.com/api/films/${id}`;
 req
   .get(url, { encoding: "utf-8" })
 
-  .on("data", (body) => {
+  .on("body", (body) => {
     const response = JSON.parse(body);
     console.log(response.title);
   });
