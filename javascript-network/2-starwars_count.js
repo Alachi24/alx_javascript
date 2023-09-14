487b86970b76d875ef758ef79e3d638e1f426b71
Chisaneme Aloni <chisanemealoni@gmail.com>
2023-09-14 21:50:48 +0100

    Star wars Wedge Antilles

--- javascript-network/2-starwars_count.js
+++ javascript-network/2-starwars_count.js
@@ -3,22 +3,14 @@
 const request = require("request");
 
 const url = "https://swapi-api.alx-tools.com/api/films/";
-const character_id = 18;
+const character_id = "18";
 
 request(url, function (error, response, body) {
-  const films = JSON.parse(body).results;
-
-  let count = 0;
-  for (let i = 0; i < 11; i++) {
-    const characters = films[i].characters;
-    if (
-      characters.includes(
-        `https://swapi-api.alx-tools.com/api/people/${character_id}/`
-      )
-    ) {
-      count++;
-    }
-  }
+  const count = JSON.parse(body).results.filter((film) =>
+    film.characters.includes(
+      `https://swapi-api.alx-tools.com/api/people/${character_id}/`
+    )
+  ).length;
 
   console.log(`${count}`);
 });
